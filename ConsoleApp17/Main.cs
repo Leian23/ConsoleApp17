ac56ec263fdc826ddcf7868013edef7063db29bf
Leandro C. Ayeras <Leandro C. Ayeras@DESKTOP-60JT4CO>
2022-06-12 15:27:34 +0800

    Add project files.

---
+++ ConsoleApp17/Main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSharpExercises
+{
+    public class Input
+    {
+        
+            public static void Main(string[] args)
+            {
+
+                string selection = Functions.DisplayMenu();
+
+                switch (selection)
+                {
+                    case "1":
+
+                    do
+                    {
+                        List<string> dataInput = Functions.GetData();
+                        List<string> dateTime = Functions.Datetime();
+                        List<string> status = Functions.Validator();
+                        FileStream.AppendFile(dataInput, dateTime, status);
+                    }
+
+                    while (true);
+                    break;
+                    case "2":
+                        try
+                        {
+                            Console.WriteLine("Displaying your inputs in console....\n");
+                            Functions.DisplayData();
+                            Console.WriteLine("\nSuccesfully displayed");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Ivalid Input");
+                        break;
+
+                }
+
+            }
+
+
+
+
+
+
+
+
+        }
+        
+
+      
+
+        }
+    
+
---
+++ ConsoleApp17/StudentNumberTextFileStream.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSharpExercises
+{
+    public class FileStream
+    {
+        internal static string fileName = $"StudentAttendance.txt";
+         
+
+        internal static void AppendFile(List<string> dataInput, List<string> dateTime, List<string> status)
+        {
+
+            Queue<string> deferredLines = new Queue<string>();
+
+            using (StreamWriter file = File.AppendText(fileName))
+            {
+                
+                    WriteDataInFile(file, dataInput, dateTime, status);
+
+            }
+                
+        }
+
+        internal static void WriteDataInFile(StreamWriter file, List<string> dataInput, List<string> dateTime, List <string> status)
+        {
+            
+            foreach (var data in dataInput)
+            {
+                file.Write(data);
+
+                if (data.Length == 2)
+                {
+                    file.Write("");
+                }
+                else
+                {
+                    foreach (var data1 in dateTime)
+                    {
+                        file.Write($"\t\t{data1}");
+                    }
+
+                    foreach (var data2 in status)
+                    {
+                        file.Write($"\t\t{data2}\n");
+                    }
+                }
+               
+            }
+
+        }
+
+
+
+
+        internal static List<string> ReadFile()
+        {
+            List<string> dataContent = new List<string>();
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                string line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    dataContent.Add(line);
+                    line = sr.ReadLine();
+                }
+            }
+
+            return dataContent;
+        }
+    }
+}
